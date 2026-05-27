808d73c815d58cf70c779078e72fb4310b3304fe
HarshP-2610 <harsh261120@gmail.com>
2026-05-27 21:28:13 +0530

    Update ENV Before Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const B = {
   50: "#eff6ff", 100: "#dbeafe", 200: "#bfdbfe",
